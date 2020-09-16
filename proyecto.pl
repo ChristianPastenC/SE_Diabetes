54d2e294c7f6bdefb1d00678e531ca39ea055019
ChristianPC <pastencristian2@gmail.com>
2020-09-16 00:16:12 -0500

    Calculo de IMC

---
+++ proyecto.pl
@@ -0,0 +1,68 @@
+% This Buffer is for notes you don't want to save.
+% If you want to create a file, visit that file with C-x C-f,
+% then enter the text in that file's own buffer.
+
+%Importación de la libreria grafica
+:- use_module(library(pce)).
+:- pce_image_directory('./assets').
+:- use_module(library(pce_style_item)).
+
+resource(tabla,image,image('tablaIMC.jpeg')).
+
+%Funcion para el redondeo de numeros decimales
+truncate(X,N,Result):- X >= 0, Result is floor(10^N*X)/10^N, !.
+%Funcion para el calculo de IMC
+%imc(CM, KG,MC):-
+    %M is CM/100,
+    %Aux is M*M,
+    %truncate(KG/Aux,2,MC).
+
+%Definicion de la funcion principal
+main:-
+    new(Window, dialog('Sistema experto diabetes')),
+    send(Window, size, size(750,600)),
+    new(L, dialog_group('')),
+    new(R, dialog_group('')),
+    send(Window,append,L),
+    send(Window, append, R, right),
+
+    new(Fig, figure),
+    new(Bitmap, bitmap(resource(tabla),@on)),
+    send(Bitmap, name, 1),
+    send(Fig,display,Bitmap),
+    send(Fig,status,1),
+    send(L,display,Fig),
+
+    new(@pesoItem, text_item('Peso en Kilogramos:')),
+    send(R,append,@pesoItem),
+    new(@alturaItem, text_item('Altura en centimetros')),
+    send(R, append, @alturaItem),
+    new(@texto, label(nombre,'Su indice de masa corporal es:')),
+    send(R, append, @texto),
+    new(@indice, label(nombre,'')),
+    send(R, append, @indice),
+    new(Calc, button('Calcular IMC', message(@prolog,imc))),
+    send(R, append, Calc),
+
+    new(@inicio,button('Iniciar Diagnostico', message(@prolog,iniciar))),
+    send(Window, append, @inicio),
+
+    send(Window,open_centered).
+
+imc:-
+    get(@pesoItem,selection,AuxKG),
+    get(@alturaItem,selection,AuxCM),
+    atom_number(AuxKG, KG),
+    atom_number(AuxCM, CM),
+    M is CM/100, Aux is M*M,
+    send(@indice, selection((KG/Aux))).
+
+
+
+
+
+
+
+
+
+
