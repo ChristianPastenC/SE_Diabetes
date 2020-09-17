944969bb20bca459392813c65710bbde3333b160
ChristianPC <pastencristian2@gmail.com>
2020-09-16 21:35:08 -0500

    Usarprimera regla

--- proyecto.pl
+++ proyecto.pl
@@ -17,15 +17,32 @@ resource(tabaco, image, image('tabaco.jpg')).
 %Funcion para el redondeo de numeros decimales
 truncate(X,N,Result):- X >= 0, Result is floor(10^N*X)/10^N, !.
 
-%Definicion de la funcion principal
-main:-
-    new(Window, dialog('Sistema experto diabetes')),
-    send(Window, size, size(750,600)),
+%Funcion para mostrar una imagen
+show_picture(Window, Image):-
+    new(Fig, figure),
+    new(Bitmap, bitmap(resource(Image),@on)),
+    send(Bitmap, name, 1),
+    send(Fig,display,Bitmap),
+    send(Fig,status,1),
+    send(Window,display,Fig).
+
+
+%Regla para obesidad
+reglaObesidad:-
+    new(Window, dialog('Indice de Masa Corporal')),
+    send(Window, size, size(750,300)),
     new(L, dialog_group('')),
     new(R, dialog_group('')),
     send(Window,append,L),
     send(Window, append, R, right),
-
+    new(Preg, label(nombre, 'El Indice de masa corporal (IMC) es un indicador de gordura confiable para las personas\n
+Se considera que un adulto está en sobrepeso si tiene un
+IMC de entre los 25 y 29.9 kg/m2 y es
+obeso si posee un IMC superior a 30kg/m2.\n
+(IMC = peso[kg]/estatura[m2]).
+')),
+    send(R, display, Preg),
+    show_picture(L,tabla),
     new(@pesoItem, text_item('Peso en Kilogramos:')),
     send(R,append,@pesoItem),
     new(@alturaItem, text_item('Altura en centimetros')),
@@ -36,11 +53,7 @@ main:-
     send(R, append, @indice),
     new(Calc, button('Calcular IMC', message(@prolog,imc))),
     send(R, append, Calc),
-
-    new(@inicio,button('Iniciar Diagnostico', message(@prolog,iniciar))),
-    send(Window, append, @inicio),
-
-    send(Window,open_centered).
+    send(Window, open_centered).
 
 %Funcion Para el calculo de IMC
 imc:-
@@ -52,14 +65,15 @@ imc:-
     send(@indice, selection((KG/Aux))).
 
 
-%Funcion para mostrar una imagen
-show_picture(Window, Image):-
-    new(Fig, figure),
-    new(Bitmap, bitmap(resource(Image),@on)),
-    send(Bitmap, name, 1),
-    send(Fig,display,Bitmap),
-    send(Fig,status,1),
-    send(Window,display,Fig).
+%Definicion de la funcion principal
+main:-
+    new(Window, dialog('Sistema Diabetes')),
+    send(Window, size, size(250,250)),
+    new(@inicio,button('Iniciar Diagnostico', message(@prolog,reglaObesidad))),
+    send(Window, append, @inicio),
+    new(BtnSalir, button('Salir', and(message(Window,destroy),message(Window,free)))),
+    send(Window,display,BtnSalir, point(200,230)),
+    send(Window,open_centered).
 
 
 
